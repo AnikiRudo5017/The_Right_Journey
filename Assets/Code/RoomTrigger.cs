a299d1f3385be46445b9c166c3b10e16d6d27d19
AnikiRudo5017 <nguyenvu5017@gmail.com>
2025-07-24 21:51:01 +0700

    logic cua

---
+++ Assets/Code/RoomTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public RoomDoorController doorController;
+
+    private bool hasEntered = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasEntered && collision.CompareTag("Player"))
+        {
+            hasEntered = true;
+           
+            StartCoroutine(DelayedOpenDoor());
+
+            
+            doorController.CloseDoor();
+        }
+    }
+
+    IEnumerator DelayedOpenDoor()
+    {
+        yield return new WaitForSeconds(3f);
+        doorController.OpenDoor();
+    }
+}
