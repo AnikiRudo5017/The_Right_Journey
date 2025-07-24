a1e615f3a7230faa20f3d7be4841ad716ef2f121
kosamon <anhntph49631@gmail.com>
2025-07-24 21:37:05 +0700

    logic cua

---
+++ Assets/Code/RoomDoorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RoomDoorController : MonoBehaviour
+{
+    public Tilemap openDoorTilemap;     // Cửa mở
+    public Tilemap closeDoorTilemap;    // Cửa đóng
+
+    private bool doorClosed = false;
+
+    private void Start()
+    {
+        openDoorTilemap.gameObject.SetActive(true);
+        closeDoorTilemap.gameObject.SetActive(false);
+    }
+
+    public void CloseDoor()
+    {
+        if (!doorClosed)
+        {
+            doorClosed = true;
+            openDoorTilemap.gameObject.SetActive(false);
+            closeDoorTilemap.gameObject.SetActive(true);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        closeDoorTilemap.gameObject.SetActive(false);
+        openDoorTilemap.gameObject.SetActive(true);
+        doorClosed = false;
+    }
+}
